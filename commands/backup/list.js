a567c4dbbf43b650ccf8540ca418bd71a76543e3
legend-js <75608223+legend-js-dev@users.noreply.github.com>
2021-06-21 19:20:33 +0545

    xd

--- commands/backup/list.js
+++ commands/backup/list.js
@@ -13,7 +13,7 @@ module.exports = {
     .setTitle("Backup list")
     .setDescription(`Server name - Backup ID - emojiCount\n${arr.join("\n")}`)
     embed.setAuthor(message.author.tag, message.author.displayAvatarURL())
-    embed.setFooter(message.guild.name + " | made by LΣGΣПD#0001 & ant#0768", message.guild.iconURL());
+    embed.setFooter(message.guild.name + " | made by legendjs#0001 & ant#0768", message.guild.iconURL());
 embed.setColor("GREEN")
 embed.setTimestamp()
             return message.channel.send({ embed: embed });
