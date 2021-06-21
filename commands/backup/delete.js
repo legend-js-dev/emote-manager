b64d6a60467ab6e7747b1e97cbd712d37ba55cf6
legend-js <75608223+legend-js-dev@users.noreply.github.com>
2021-06-21 19:21:16 +0545

    xd

--- commands/backup/delete.js
+++ commands/backup/delete.js
@@ -11,7 +11,7 @@ module.exports = {
                 No.setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
                 No.setDescription(`:x: | **backup Not Found**`)
                 No.setColor("#FF0000")
-                No.setFooter(message.guild.name + " | made by LΣGΣПD#0001 & ant#0768 & ant#0768", message.guild.iconURL());
+                No.setFooter(message.guild.name + " | made by legendjs#0001 & ant#0768", message.guild.iconURL());
                 No.setThumbnail(message.guild.iconURL())
 
             if (!data) return message.channel.send({ embed: No });
@@ -25,7 +25,7 @@ module.exports = {
         let embed = new Discord.MessageEmbed()
                 embed.setAuthor(message.author.tag, message.author.displayAvatarURL())
                 embed.setDescription(`**The backup has been deleted!** `)
-                embed.setFooter(message.guild.name + " | made by LΣGΣПD#0001 & ant#0768", message.guild.iconURL());
+                embed.setFooter(message.guild.name + " | made by legendjs#0001 & ant#0768", message.guild.iconURL());
 embed.setColor("GREEN")
 embed.setTimestamp()
             return message.channel.send({ embed: embed });
@@ -33,7 +33,7 @@ embed.setTimestamp()
             let embed = new Discord.MessageEmbed()
                 embed.setAuthor(message.author.tag, message.author.displayAvatarURL())
                 embed.setDescription(`:x: | **The backup was not found!**`)
-                embed.setFooter(message.guild.name + " | made by LΣGΣПD#0001 & ant#0768", message.guild.iconURL());
+                embed.setFooter(message.guild.name + " | made by legendjs#0001 & ant#0768", message.guild.iconURL());
                 embed.setColor("#FF0000")
                 embed.setTimestamp()
 
