66aa31b9362132c7e1460592a02c29515cb13614
legend-js <75608223+legend-js-dev@users.noreply.github.com>
2022-01-22 17:14:37 +0545

    xd

--- commands/emotes/big.js
+++ commands/emotes/big.js
@@ -1,12 +1,31 @@
+const {Util} = require("discord.js");
 module.exports = {
-  name: "big",
-  run: async(client, message, args) => {
-    const discord = require("discord.js");
-    if (!args[0]) return message.channel.send("emote is a required argument that is missing.");
-    const emo = discord.Util.parseEmoji(args[0]);
-    if (!emo.name || !emo.id) return message.channel.send("Invalid emote argument");
-    const res = `${emo.name}: https://cdn.discordapp.com/emojis/${emo.id}.${emo.animated ? "gif" : "png"}`;
-    message.channel.send(res);
-    
-  }
+    name: "big",
+    description: "Enlarge an emoji",
+    options: [
+        {
+            name: "emoji",
+            type: "STRING",
+            description: "The emoji to enlarge",
+            required: true
+        }
+    ],
+    type: "CHAT_INPUT",
+    run: async (client, interaction, args) => {
+        if (!args[0]) 
+            return interaction.followUp({content: ":x: **Emote is a required argument that is missing.**"});
+        
+        const emo = Util.parseEmoji(args[0]);
+        if (! emo.name || ! emo.id) 
+            return interaction.followUp({content: "Invalid emote argument"});
+        
+        const res = `${
+            emo.name
+        }: https://cdn.discordapp.com/emojis/${
+            emo.id
+        }.${
+            emo.animated ? "gif" : "png"
+        }`;
+        interaction.followUp({content: res});
+    }
 }
