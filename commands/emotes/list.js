6b4abece6b5b5a3357744c3df0a316c1d61e6261
legend-js <75608223+legend-js-dev@users.noreply.github.com>
2022-01-22 17:14:56 +0545

    xd

--- commands/emotes/list.js
+++ commands/emotes/list.js
@@ -1,56 +1,100 @@
+const {
+    MessageButton,
+    MessageActionRow,
+    MessageEmbed,
+    Permissions,
+    Util
+} = require("discord.js");
+
 module.exports = {
-  name: "list",
-  run: async(client, message, args) => {
-    const Discord = require("discord.js");
-    let list = [];
-    let emojis = message.guild.emojis.cache.array();
-    if (emojis.size === 0) return message.channel.send("There are no emojis in this server");
-     emojis = emojis.map((e, i) => `${i + 1}. ${e} \\${e}`);
-    for (var i = 0; i < emojis.length; i += 10) {
-      const items = emojis.slice(i, i + 10);
-      list.push(items.join("\n"));
+    name: "list",
+    description: "Shows a list of the Emojis in this Server",
+    type: "CHAT_INPUT",
+    run: async (client, interaction, args) => {
+        let list = [];
+        let emojis = [];
+        interaction.guild.emojis.cache.forEach(x => emojis.push(x));
+        if (emojis.size === 0) 
+            return interaction.followUp({content: "There are no emojis in this server"});
+        
+
+        emojis = emojis.map((e, i) => `${
+            i + 1
+        }. ${e} \\${e}`);
+        for (var i = 0; i < emojis.length; i += 10) {
+            const items = emojis.slice(i, i + 10);
+            list.push(items.join("\n"));
+        }
+        let page = 0;
+        let button1 = new MessageButton().setEmoji("⬅️").setStyle("PRIMARY").setCustomId("previous");
+        let button2 = new MessageButton().setEmoji("⏹").setStyle("DANGER").setCustomId("stop");
+        let button3 = new MessageButton().setEmoji("➡️").setStyle("PRIMARY").setCustomId("next");
+        let buttons = new MessageActionRow().addComponents([button1, button2, button3]);
+        refreshButtons(button1, button2, button3);
+        let e = new MessageEmbed().setDescription(list[page]).setFooter({
+                text: `Page ${
+                page + 1
+            } of ${
+                list.length
+            } (${
+                emojis.length
+            } entries)`
+        }).setColor("YELLOW");
+        const msg = await interaction.followUp({embeds: [e], components: [buttons]});
+        let doing = true;
+        while (doing) {
+            let r;
+            const filter = function (button) {
+                if (button.user !== interaction.user) 
+                    return button.deferReply();
+                
+                return ["previous", "stop", "next"].includes(button.customId) && button.user === interaction.user;
+            };
+            try {
+                r = await msg.awaitMessageComponent({filter, max: 1, time: 20000, errors: ["time"]})
+            } catch (error) {
+                console.log(error);
+                return interaction.followUp("Command timed out.")
+            }
+            const u = interaction.user;
+            if (r.customId == "next") {
+                page++;
+                r.deferReply();
+                let newEmbed = new MessageEmbed().setDescription(list[page]).setFooter({
+                        text: `Page ${
+                        page + 1
+                    } of ${
+                        list.length
+                    } (${
+                        emojis.length
+                    } entries)`
+                }).setColor("YELLOW");
+                msg.edit({embeds: [newEmbed], components: [buttons]});
+            } else if (r.customId == "previous") {
+                page--;
+                r.deferReply() && refreshButtons(button1, button2, button3)
+                let newEmbed = new Discord.MessageEmbed().setDescription(list[page]).setFooter(`Page ${
+                    page + 1
+                } of ${
+                    list.length
+                } (${
+                    emojis.length
+                } entries)`).setColor("YELLOW");
+                msg.edit(newEmbed);
+            } else if (r.customId == "stop") {
+                button1.setDisabled(true) && button2.setDisabled(true) && button3.setDisabled(true)
+                return;
+            }
+        }
+
+        function refreshButtons(button1, button2, button3) {
+            if (! list[page - 1]) 
+                button1.setDisabled(true);
+             else if (! list[page + 1]) 
+                button3.setDisabled(true)
+
+            
+
+        }
     }
-    const symbols = ["➡️", "⏹", "⬅️"];
-    let page = 0;
-    let e  = new Discord.MessageEmbed()
-    .setDescription(list[page])
-    .setFooter(`Page ${page + 1} of ${list.length} (${emojis.length} entries)`)
-    .setColor("YELLOW");
-    const msg = await message.channel.send({ embed: e });
-    symbols.forEach(symbol => msg.react(symbol));
-    let doing = true;
-    while (doing) {
-    let r;
-    const filter = (r, u) => symbols.includes(r.emoji.name) && u.id == message.author.id;
-    try { r = await msg.awaitReactions(filter, { max: 1, time: 20000, errors: ["time"] }) }
-    catch { return message.channel.send("Command timed out.") }
-    const u = message.author;
-    r = r.first();
-    if (r.emoji.name == symbols[0]) {
-      if (!list[page + 1]) msg.reactions.resolve(r.emoji.name).users.remove(u.id).catch(err => {});
-      else {
-      page++;
-      msg.reactions.resolve(r.emoji.name).users.remove(u.id).catch(err => {});
-      let newEmbed = new Discord.MessageEmbed()
-     .setDescription(list[page])
-     .setFooter(`Page ${page + 1} of ${list.length} (${emojis.length} entries)`)
-     .setColor("YELLOW");
-     msg.edit(newEmbed);
-      }
-    } else if (r.emoji.name == symbols[2]) {
-      if (!list[page - 1]) msg.reactions.resolve(r.emoji.name).users.remove(u.id).catch(err => {});
-      else {
-      page--;
-      msg.reactions.resolve(r.emoji.name).users.remove(u.id).catch(err => {});
-      let newEmbed = new Discord.MessageEmbed()
-     .setDescription(list[page])
-     .setFooter(`Page ${page + 1} of ${list.length} (${emojis.length} entries)`)
-     .setColor("YELLOW");
-     msg.edit(newEmbed);
-      }
-    } else if (r.emoji.name == symbols[1]) {
-       msg.reactions.removeAll();
-       return;
-      }
-  }}
 }
