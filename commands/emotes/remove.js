20eff017b93fe577a56ecfa3cc1683e191e12487
legend-js <75608223+legend-js-dev@users.noreply.github.com>
2022-01-22 18:57:38 +0545

    xd

--- commands/emotes/remove.js
+++ commands/emotes/remove.js
@@ -2,28 +2,36 @@ const Discord = require('discord.js')
 
 module.exports = {
   name: "remove",
-  run: async (client, message, args) => {
-    if (!message.member.hasPermission("MANAGE_EMOJIS")) {
-return message.channel.send(`:x: | **You Don't Have Permission To Use This Command**`)
+  description: "Remove an emoji from the server",
+  options: [
+    {
+      name: "emoji",
+      description: "The emoji you want to remove",
+      type: "STRING",
+      required: true
+    }
+  ],
+  type: "CHAT_INPUT",
+  run: async (client, interaction, args) => {
+    if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_EMOJIS)) {
+return interaction.followUp({ content: `:x: | **You don't have Permission permissions to use this Command**` })
 }
-if (!message.guild.me.hasPermission("MANAGE_EMOJIS")) {
-return message.channel.send(`:x: | **I Don't Have Permission To manage emojis**`)
+if (!interaction.guild.me.permissions.has(Permissions.FLAGS.MANAGE_EMOJIS)) {
+return interaction.followUp({ content: `:x: | **I don't have permission to Manage Emojis**` })
 }
-if (!args[0]) return message.channel.send("emote is a required argument that is missing.");
 let emo = args[0].match(/(?<=<?a?:?\w{2,32}:)\d{17,19}(?=>?)/gi)[0]
-if (!emo) return message.channel.send("emote is a required argument that is missing.")
-if (message.guild.emojis.cache.get(emo)) {
-  emo = message.guild.emojis.cache.get(emo)
+if (!emo) return interaction.followUp({ content: ":x: | **Emote is a required argument that is missing.**" })
+if (interaction.guild.emojis.cache.get(emo)) {
+  emo = interaction.guild.emojis.cache.get(emo)
 } else {
-  return message.channel.send(":x: | Emoji not found")
+  return interaction.followUp({ content: ":x: | **Emoji not found**" })
 }
-if (!emo.name || !emo.id) return message.channel.send("Invalid emote argument");
-console.log(emo)
+if (!emo.name || !emo.id) return interaction.followUp({ content: ":x: | **Invalid emote argument**" });
    try {
      emo.delete()
-     message.channel.send("**The Emoji has been removed**")
+     interaction.followUp({ content: "**The Emoji has been removed**" })
    } catch (err) {
-     message.channel.send(":x: | **An Error occured**")
+     interaction.followUp({ content: ":x: | **An Error occured**" })
    }
   }
 }
